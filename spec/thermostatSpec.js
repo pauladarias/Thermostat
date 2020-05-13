b5b67c05e8e5ccc5f953f96f16c2448daabe4ab9
pauladarias <pauladarias@gmail.com>
2020-05-13 14:43:52 +0100

    up and down functions added adn tests passed

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,7 +8,16 @@
       thermostat = new Thermostat();
     })
     it('starts at 20 min', function(){
-      expect(thermostat.temperature).toEqual(20);
+      expect(thermostat.getCurrentTemperature()).toEqual(20);
+    });
+    it('increases the temperature with up()', function(){
+      thermostat.up();
+      expect(thermostat.getCurrentTemperature()).toEqual(21);
+    });
+    it('decreases the temperature with dowm()', function() {
+      thermostat.down();
+      expect(thermostat.getCurrentTemperature()).toEqual(19);
+
     });
 
   });
